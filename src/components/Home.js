ee5f818a74caf9a5b79e7ea03361f31cae76927d
Nawed Ali <nawed2611@gmail.com>
2022-06-10 03:03:06 +0530

    Add Home

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Typography from '@mui/material/Typography';
+
+const Home = () => {
+  return (
+    <div>
+      {/* <Typography variant="h3">Search For any City and get their Weather Info</Typography>
+      <Typography variant="h6">Super Easy to Search </Typography> */}
+    </div>
+  )
+}
+
+export default Home
